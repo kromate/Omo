4a3ff80390f554195ab2af77340f14f8ea3b2b3c
Anthony Akpan <52849936+kromate@users.noreply.github.com>
2022-03-11 00:01:10 +0100

    modal base added

---
+++ modal/modalBase.vue
@@ -0,0 +1,26 @@
+// place this component in a layout page
+
+<template>
+	<div>
+		<component :is="modal.elem" v-for="modal in allModals" :key="modal.elem.name" :show-modal="modal.state.value" @close="modal.close" />
+	</div>
+</template>
+
+<script >
+
+import {  globalModal } from '@/composables/core/modals'
+
+export default {
+	name: 'ModalBase',
+	setup () {
+
+		const allModals = globalModal()
+
+		console.log(allModals)
+
+		
+		
+		return { allModals }
+	}
+}
+</script>
